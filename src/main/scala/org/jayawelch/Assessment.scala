c0481495f4017670ca3c5e7712a3fa42c3fc2e9f
Jay Welch <ajaywelch@gmail.com>
2021-02-11 14:45:28 -0500

    Testing new token authentication

--- src/main/scala/org/jayawelch/Assessment.scala
+++ src/main/scala/org/jayawelch/Assessment.scala
@@ -3,7 +3,15 @@ package org.jayawelch
 import org.jayawelch.hackerrank.HackerRankSolutions
 
 object Assessment {
+  def toBinary(i : Long) = {
+    def loop(q: Long, remainders: List[Long]): String =
+      if (q / 2 == 0) (q % 2 :: remainders).mkString
+      else loop(q / 2, q % 2 :: remainders)
+
+    loop(i, List.empty[Long])
+  }
+
   def main(args: Array[String]): Unit = {
-    println(HackerRankSolutions.rearrange("aaazzzzzbcdegggg", 2))
+    println(toBinary(6))
   }
 }
