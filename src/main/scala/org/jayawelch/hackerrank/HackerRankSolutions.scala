28272b6e485de1abd465d4cd345689cb7cb1a934
Jay Welch <ajaywelch@gmail.com>
2021-07-08 10:27:05 -0400

    Redo of rearranging string

--- src/main/scala/org/jayawelch/hackerrank/HackerRankSolutions.scala
+++ src/main/scala/org/jayawelch/hackerrank/HackerRankSolutions.scala
@@ -5,49 +5,42 @@ import scala.collection.immutable.TreeMap
 
 object HackerRankSolutions {
 
-  def rearrange(s: String, k: Int): String =
+  def rearrange(s: String, k: Int): String = {
+    val sorted = s.toSeq.sorted(Ordering[Char].reverse)
+    sorted.subSequence(0, sorted.lastIndexOf(sorted(0))).toString.grouped(k)
     arrange(
-      s.toList
-        .foldLeft(TreeMap.empty[Char, Int](Ordering[Char].reverse)) { (map, ch) =>
-          map.get(ch).fold(map.+(ch -> 1))(l => map.+(ch -> (l + 1)))
-        }.toList,
+      s.toSeq.sorted(Ordering[Char].reverse),
       k,
       ""
     )
+  }
 
 
-  private def lookAhead(l: List[(Char, Int)]): Option[(Char, List[(Char, Int)])] =
-    l match {
-      case Nil => None
-
-      case x :: xs if x._2 == 1 =>
-        Some((x._1, xs))
-
-      case x :: xs =>
-        Some((x._1, (x._1, x._2 - 1) :: xs))
-    }
 
   @tailrec
-  private def arrange(l: List[(Char, Int)], k: Int, acc: String): String =
-    l match {
-      case Nil => acc
-
-      case x :: xs if x._2 < k =>
-        arrange(xs, k, acc + (x._1.toString * x._2))
-
-      case x :: xs if xs.nonEmpty =>
-        val chList = (x._1.toString * x._2).toList.sliding(k, k).toList
-        val (rearranged, rest) = chList.take(chList.size - 1)
-          .foldLeft((acc, xs)) { (tup, subList) =>
-            lookAhead(tup._2).fold((tup._1, List.empty[(Char, Int)]))(ct => (tup._1 + subList.mkString + ct._1, ct._2))
+  private def arrange(sorted: Seq[Char], k: Int, acc: String): String =
+    if (sorted.isEmpty)
+      acc
+    else {
+      val idx = sorted.lastIndexOf(sorted.head)
+      if (idx < sorted.length) {
+        val (work, rest) = sorted.splitAt(idx + 1)
+        val grouped = work.grouped(k).toSeq
+        val (ready, leftOver) = grouped.tail
+          .foldLeft((acc ++ grouped.head.toString, rest)){(tuple2, seq) =>
+            if (tuple2._2.nonEmpty)
+              (tuple2._1 + tuple2._2.head.toString + seq.toString, tuple2._2.tail)
+            else if (tuple2._1.last != seq.head)
+              (tuple2._1 + seq.toString, tuple2._2)
+            else
+              (tuple2._1, tuple2._2)
           }
-        if (rearranged.charAt(rearranged.length - 1) != chList.head.head)
-          arrange(rest, k, rearranged + chList.drop(chList.size - 1).head.mkString)
-        else
-          arrange(rest, k, rearranged)
+        arrange(leftOver, k, ready)
+      } else {
+        val (keep, _) = sorted.splitAt(if (k < sorted.length) k else sorted.length - 1)
+        arrange("", k, acc + keep.toString)
+      }
 
-      case x :: _ =>
-        acc + (x._1.toString * k)
     }
 
 }
--- src/test/scala/org/jayawelch/hackerrank/SolutionsSpec.scala
+++ src/test/scala/org/jayawelch/hackerrank/SolutionsSpec.scala
@@ -7,4 +7,12 @@ class SolutionsSpec extends AnyFlatSpec with Matchers {
   "Solutions" should "rearrange abababcczzzzz to zzczzczbbabaa" in {
     HackerRankSolutions.rearrange("abababcczzzzz", 2) should be ("zzczzczbbabaa")
   }
+
+  "Solutions" should "rearrange azzzzz to zzazz" in {
+    HackerRankSolutions.rearrange("azzzzz", 2) should be ("zzazz")
+  }
+
+  "Solutions" should "rearrange zzzzz to zz" in {
+    HackerRankSolutions.rearrange("zzzzz", 2) should be ("zz")
+  }
 }
